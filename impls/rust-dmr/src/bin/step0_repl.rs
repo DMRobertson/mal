995afce5608f27a84411300c37fd66aaa5397d22
David Robertson <david.m.robertson1@gmail.com>
2020-04-15 19:31:54 +0100

    Step 0: trivial repl

--- impls/rust-dmr/src/bin/step0_repl.rs
+++ impls/rust-dmr/src/bin/step0_repl.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+
+fn read(line: &str) -> &str {
+    line
+}
+
+fn eval(line: &str) -> &str {
+    line
+}
+
+fn print(line: &str) -> &str {
+    line
+}
+
+fn rep(line: &str) -> &str {
+    print(eval(read(&line)))
+}
+
+fn main() -> std::io::Result<()> {
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+    let mut line = String::new();
+    loop {
+        const PROMPT: &[u8] = b"user> ";
+        stdout.write(PROMPT)?;
+        stdout.flush()?;
+        let bytes_read = stdin.read_line(&mut line)?;
+        if bytes_read == 0 {
+            break;
+        }
+        stdout.write(rep(&line).as_bytes())?;
+        line.clear();
+    }
+    Ok(())
 }
