129914c7268bae37fc049c324e69600b2a526297
David <david.m.robertson1@gmail.com>
2020-07-05 16:47:07 +0100

    Remove debug from step6

--- impls/rust-dmr/src/bin/step6_file.rs
+++ impls/rust-dmr/src/bin/step6_file.rs
@@ -2,11 +2,8 @@ use rust_dmr_mal::{cmdline, environment};
 use std::rc::Rc;
 
 fn main() -> Result<(), cmdline::Error> {
-    log::debug!("make env");
     let env = Rc::new(environment::Environment::default());
-    log::debug!("read prelude");
-    environment::read_prelude(&env).expect("error during setup");
-    log::debug!("allowing eval in this environment");
+    environment::read_prelude(&env).expect("error reading prelude");
     environment::add_eval(&env);
     let args = std::env::args().collect();
     cmdline::launch(args, &env)
