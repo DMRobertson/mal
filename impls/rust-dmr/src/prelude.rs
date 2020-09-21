3a7a5b2a338d4c6009009ffb8a5499de467b1348
David <david.m.robertson1@gmail.com>
2020-09-21 03:11:55 +0100

    Change load-file's mal implementation to eval one thing at a time
    Since each call to "primitive eval" is logged, this helps make debugging easier

--- impls/rust-dmr/src/prelude.rs
+++ impls/rust-dmr/src/prelude.rs
@@ -1,5 +1,5 @@
 pub const PRELUDE: &str = r#"
 (def! not (fn* (a) (if a false true)))
-(def! load-file (fn* (f) (eval (read-string (str "(do " (slurp f) "\nnil)")))))
+(def! load-file (fn* (f) (do (map eval (rest (read-string (str "(do " (slurp f) "\nnil)")))) nil)))
 (defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw "odd number of forms to cond")) (cons 'cond (rest (rest xs)))))))
 "#;
