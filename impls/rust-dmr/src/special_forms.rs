DMRobertson/mal

commit f50a530e516c55ecf3200ba012d8dc97ac1eb7b3

Allow defining variadic functions
This completes step 4 which I think was pretty hard!
I'm not sure you can do all that much with such functions yet---we don't have the ability to loop or unpack.

impls/rust-dmr/src/special_forms.rs
@@ -1,9 +1,11 @@
-use crate::types::{truthy, Arity, Closure, MalList, MalObject, MalSymbol};
+use crate::types::{
+    truthy, Arity, BadClosureParameters, Closure, ClosureParameters, MalList, MalObject, MalSymbol,
+};
 use itertools::Itertools;
 
 use crate::environment::Environment;
 use crate::evaluator::{Error, Result, EVAL};
-use crate::special_forms::FnError::ParameterNotASymbol;
+use crate::special_forms::FnError::{BadVariadic, ParameterNotASymbol};
 use std::convert::TryFrom;
 use std::rc::Rc;
 
@@ -108,9 +110,13 @@ pub enum FnError {
     WrongArgCount(usize),
     ParametersNotGivenAsList,
     ParameterNotASymbol,
+    BadVariadic(BadClosureParameters),
 }
 
 pub fn apply_fn(args: &[MalObject], env: &Rc<Environment>) -> Result {
+    // Start by checking that we've been given the right kind of arguments.
+    // We expect exactly two arguments. The first, a parameters list, should be a sequence of symbols.
+    // The second, the expression body of the function we're defining, is any MalObject.
     let (parameters, body) = match args.len() {
         2 => Ok((&args[0], &args[1])),
         n => Err(Error::Fn(FnError::WrongArgCount(n))),
@@ -124,9 +130,10 @@ pub fn apply_fn(args: &[MalObject], env: &Rc<Environment>) -> Result {
     };
     let parameters: std::result::Result<Vec<MalSymbol>, _> =
         parameters.iter().map(extract_symbol).collect();
+
     let parameters = parameters.map_err(Error::Fn)?;
     let closure = Closure {
-        parameters,
+        parameters: ClosureParameters::new(parameters).map_err(|e| Error::Fn(BadVariadic(e)))?,
         body: body.clone(),
         parent: env.clone(),
     };

impls/rust-dmr/src/evaluator.rs
@@ -50,7 +50,7 @@ pub(crate) fn EVAL(ast: &MalObject, env: &Rc<Environment>) -> Result {
 }
 
 fn evaluate_ast(ast: &MalObject, env: &Rc<Environment>) -> Result {
-    log::debug!("eval_ast {:?}", ast);
+    log::trace!("evaluate_ast {:?}", ast);
     match ast {
         MalObject::Symbol(s) => fetch_symbol(s, env),
         MalObject::List(list) => evaluate_sequence_elementwise(list, env).map(MalObject::wrap_list),
@@ -120,12 +120,19 @@ pub fn call_primitive(func: &'static PrimitiveFn, args: &[MalObject]) -> Result 
 
 fn call_closure(func: &Closure, args: &[MalObject]) -> Result {
     log::debug!("Call closure {:?} with {:?}", func, args);
-    func.arity()
+    func.parameters
+        .arity()
         .validate_for(args.len(), "closure")
         .map_err(Error::BadArgCount)?;
     let env = Environment::spawn_from(&func.parent);
-    for (key, value) in func.parameters.iter().zip(args) {
+
+    let (positional, rest) = args.split_at(func.parameters.positional.len());
+    for (key, value) in func.parameters.positional.iter().zip(positional) {
         env.set(key.clone(), value.clone());
+    }
+    if let Some(rest_key) = &func.parameters.others {
+        let rest = rest.iter().map(|obj| obj.clone()).collect();
+        env.set(rest_key.clone(), MalObject::wrap_list(rest));
     }
     EVAL(&func.body, &env)
 }

impls/rust-dmr/src/printer.rs
@@ -82,7 +82,7 @@ fn print_map_contents(map: &MalMap) -> String {
 fn print_closure(f: &Closure) -> String {
     format!(
         "(fn* ({}) {})",
-        f.parameters.iter().map(|s| &s.name).join(" "),
+        f.parameters,
         pr_str(&f.body, PrintMode::ReadableRepresentation)
     )
 }

impls/rust-dmr/src/types.rs
@@ -107,16 +107,72 @@ impl fmt::Debug for PrimitiveFn {
     }
 }
 
-pub struct Closure {
-    pub parameters: Vec<MalSymbol>,
-    pub body: MalObject,
-    pub parent: Rc<Environment>,
+#[derive(Debug)]
+pub struct ClosureParameters {
+    pub positional: Vec<MalSymbol>,
+    pub others: Option<MalSymbol>,
 }
 
-impl Closure {
-    pub fn arity(&self) -> Arity {
-        Arity::exactly(self.parameters.len())
+impl fmt::Display for ClosureParameters {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.positional.iter().map(|s| &s.name).join(" "))?;
+        if let Some(rest) = &self.others {
+            write!(f, " & {}", rest.name)?;
+        }
+        Ok(())
     }
+}
+
+#[derive(Debug)]
+pub enum BadClosureParameters {
+    TooManyAmpersands(usize),
+    TooShortForAmpersand,
+    AmpersandPositionNotPenultimate,
+}
+
+impl ClosureParameters {
+    pub fn new(mut symbols: Vec<MalSymbol>) -> Result<Self, BadClosureParameters> {
+        let is_ampersand = |s: &&MalSymbol| s.name == "&";
+        let ampersand_count = symbols.iter().filter(is_ampersand).count();
+
+        match ampersand_count {
+            0 => Ok(ClosureParameters {
+                positional: symbols,
+                others: None,
+            }),
+            1 => {
+                if symbols.len() < 2 {
+                    return Err(BadClosureParameters::TooShortForAmpersand);
+                }
+                let penultimate = symbols.get(symbols.len() - 2).unwrap();
+                match is_ampersand(&penultimate) {
+                    false => Err(BadClosureParameters::AmpersandPositionNotPenultimate),
+                    true => {
+                        let variadic_name = symbols.pop().unwrap();
+                        let _ampersand = symbols.pop();
+                        Ok(ClosureParameters {
+                            positional: symbols,
+                            others: Some(variadic_name),
+                        })
+                    }
+                }
+            }
+            _ => Err(BadClosureParameters::TooManyAmpersands(ampersand_count)),
+        }
+    }
+
+    pub fn arity(&self) -> Arity {
+        match self.others {
+            None => Arity::exactly(self.positional.len()),
+            Some(_) => Arity::at_least(self.positional.len()),
+        }
+    }
+}
+
+pub struct Closure {
+    pub parameters: ClosureParameters,
+    pub body: MalObject,
+    pub parent: Rc<Environment>,
 }
 
 impl fmt::Debug for Closure {

commit 652bb4f5f5a4c0a179c7c939e06b90547e19d934

Have (if x y x) fully EVAL y and z
I don't fully understand why this fixes things, because the bug I was seeing involved EVALling too far. Hmmm.
But it's a clearly a bug: I'd expect (if x (prn "truthy") (prn "falsy")) to print something!

It's a shame mal doesn't have a test case to cover this!

Looks like it regressed in f50a530e when I was trying to be clever with TCO. Beware, Icarus!

impls/rust-dmr/src/special_forms.rs
@@ -110,9 +110,9 @@ pub fn apply_if(args: &[MalObject], env: &Rc<Environment>) -> Result {
         .map_err(Error::BadArgCount)?;
     let condition = EVAL(&args[0], env)?;
     if truthy(&condition) {
-        Ok(args[1].clone())
+        EVAL(&args[1], env)
     } else if args.len() == 3 {
-        Ok(args[2].clone())
+        EVAL(&args[2], env)
     } else {
         Ok(MalObject::Nil)
     }